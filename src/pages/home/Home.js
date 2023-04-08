e3ad7cec06cd921c41338b7eb67c76730beb092a
Paawccio <fordcrown2009@gmail.com>
2023-04-08 02:28:26 +0200

    Nagivation menu, homepage background

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,45 +7,69 @@ import emailIcon from "./assets/contact/email.svg";
 import messengerIcon from "./assets/contact/messenger.svg";
 
 const ContactIcon = (iconNumber) => {
-	const iconTypes = {
-		1: phoneIcon,
-		2: messengerIcon,
-		3: emailIcon
-	}
+  const iconTypes = {
+    1: phoneIcon,
+    2: messengerIcon,
+    3: emailIcon,
+  };
 
-	return (
-		<div className="container--navigation--social-icon">
-			<img src={iconTypes[iconNumber]} alt="social icon" />
-		</div>
-	);
-}
+  return (
+    <div className="container--navigation--social-icon">
+      <img src={iconTypes[iconNumber]} alt="social icon" />
+    </div>
+  );
+};
 
 export const Home = () => {
-	return (
-		<div className="container">
-			<div className="container--navigation">
-				<div className="container--navigation--logo">
-					<img src={logoSignet} alt="small logo" />
-				</div>
+  return (
+    <div className="container">
+      <div className="container--navigation">
+        <div className="container--navigation--logo">
+          <img src={logoSignet} alt="small logo" />
+        </div>
 
-				<div className="container--navigation--contact-icons">
-					{ContactIcon(1) /* phone icon */} 
-					{ContactIcon(2) /* messenger icon */}
-					{ContactIcon(3) /* email icon */}
-				</div>
-			</div>
+        <div className="container--navigation--contact-icons">
+          {ContactIcon(1) /* phone icon */}
+          {ContactIcon(2) /* messenger icon */}
+          {ContactIcon(3) /* email icon */}
+        </div>
+      </div>
 
-			<div className="container--general">
-				<div className="container--general--background">
-					<img src={logoSignet} alt="big logo" />
+      <div className="container--general">
+        <div className="container--general--background">
+          <div className="container--general--background--navbar">
+            <div className="container--general--background--navbar--items">
+              <div className="container--general--background--navbar--items--item active">
+                <span>HOME</span>
+              </div>
 
-					<div className="container--general--background--text">
-						<span>
-							<h1>PIOTR{"\n"}SUMARA</h1>
-						</span>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-}
+              <div className="container--general--background--navbar--items--item">
+                <span>O MNIE</span>
+              </div>
+
+              <div className="container--general--background--navbar--items--item">
+                <span>UMIEJĘTNOŚCI</span>
+              </div>
+
+              <div className="container--general--background--navbar--items--item">
+                <span>PORTFOLIO</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="container--general--background--text">
+            <div className="container--general--background--text--name">
+              <h1>PIOTR<br></br>SUMARA</h1>
+
+							<h2>GRAFIK KOMPUTEROWY</h2>
+            </div>
+
+            <div className="container--general--background--text--logo">
+              <img src={logoSignet} alt="big logo" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
