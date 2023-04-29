ffabd8efc4f6bfafad030e470a52be13f9c3c5ec
Paawccio <fordcrown2009@gmail.com>
2023-04-29 18:02:10 +0200

    New section with apps icons, responsive navbar & hamburger menu

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
+import { slide as Menu } from "react-burger-menu";
 
 import "./Home.scss";
 
 import logoSignet from "./assets/signet.svg";
-import plLang from "./assets/pl_lang.svg";
+// import plLang from "./assets/pl_lang.svg";
 
 import phoneIcon from "./assets/contact/phone.svg";
 import emailIcon from "./assets/contact/email.svg";
@@ -11,11 +12,23 @@ import messengerIcon from "./assets/contact/messenger.svg";
 
 import rectangle from "./assets/rectangle.svg";
 
+import acrobat from "./assets/apps/acrobat.svg";
+import figma from "./assets/apps/figma.svg";
+import illustrator from "./assets/apps/illustrator.svg";
+import impozycjoner from "./assets/apps/impozycjoner.svg";
+import indesign from "./assets/apps/indesign.svg";
+import photoshop from "./assets/apps/photoshop.svg";
+import lightroom from "./assets/apps/lightroom.svg";
+import premiere from "./assets/apps/premiere.svg";
+import procreate from "./assets/apps/procreate.svg";
+import xd from "./assets/apps/xd.svg";
+
+
 // import "animate.css/animate.min.css";
 
 // import { AnimationOnScroll } from 'react-animation-on-scroll';
 
-function Rectangles() {
+const Rectangles = () => {
   const [rectangles, setRectangles] = useState([]);
 
   const Rectangle = () => {
@@ -23,7 +36,7 @@ function Rectangles() {
   };
 
   useEffect(() => {
-    function handleResize() {
+    const handleResize = () => {
       const width = window.innerWidth;
       const newRectangles = [];
 
@@ -52,7 +65,9 @@ function Rectangles() {
   );
 }
 
-const ContactIcon = (iconNumber) => {
+const ContactIcon = (props) => {
+  const { iconNumber } = props;
+
   const iconTypes = {
     1: phoneIcon,
     2: messengerIcon,
@@ -69,6 +84,13 @@ const ContactIcon = (iconNumber) => {
 export const Home = () => {
   return (
     <div className="container">
+      <Menu>
+        <a id="home" href="/">HOME</a>
+        <a id="about" href="/about">O MNIE</a>
+        <a id="skills" href="/contact">UMIEJĘTNOŚCI</a>
+        <a id="portfolio" href="/contact">PORTFOLIO</a>
+      </Menu>
+
       <div className="sidebar">
         <div className="sidebar--logo">
           <img src={logoSignet} alt="logo" />
@@ -76,30 +98,30 @@ export const Home = () => {
 
         <div className="sidebar--social">
           <div className="sidebar--social-icons">
-            {ContactIcon(1)}
-            {ContactIcon(2)}
-            {ContactIcon(3)}
+            <ContactIcon iconNumber={1} />
+            <ContactIcon iconNumber={2} />
+            <ContactIcon iconNumber={3} />
           </div>
         </div>
       </div>
 
       <div className="main">
         <div className="main--header">
-          <div className="navigation">
-            <div className="navigation--option first active">
-              <a href="#">Home</a>
+          <div className="navigation" id="home">
+            <div className="navigation--option first">
+              <a href="#home">Home</a>
             </div>
 
             <div className="navigation--option">
-              <a href="#">O MNIE</a>
+              <a href="/">O MNIE</a>
             </div>
 
             <div className="navigation--option">
-              <a href="#">UMIEJĘTNOŚCI</a>
+              <a href="/">UMIEJĘTNOŚCI</a>
             </div>
 
             <div className="navigation--option">
-              <a href="#">PORTFOLIO</a>                
+              <a href="/">PORTFOLIO</a>                
             </div>
           </div>
         </div>
@@ -137,7 +159,7 @@ export const Home = () => {
             </div>
             
             <div className="content--right">
-              {Rectangles()}
+              <Rectangles />
             </div>
           </div>
         </div>
@@ -150,6 +172,44 @@ export const Home = () => {
 
           <div className="grid">
             <div className="grid--item">
+              <img src={acrobat} alt="Adobe Acrobat" className="grid--icon"/>
+              <h3 className="grid--title">Adobe Acrobat</h3>
+            </div>
+            <div className="grid--item">
+              <img src={photoshop} alt="Adobe Photoshop" className="grid--icon"/>
+              <h3 className="grid--title">Adobe Photoshop</h3>
+            </div>
+            <div className="grid--item">
+              <img src={illustrator} alt="Adobe Illustrator" className="grid--icon"/>
+              <h3 className="grid--title">Adobe Illustrator</h3>
+            </div>
+            <div className="grid--item">
+              <img src={premiere} alt="Adobe Premiere Pro" className="grid--icon"/>
+              <h3 className="grid--title">Adobe Premiere Pro</h3>
+            </div>
+            <div className="grid--item">
+              <img src={indesign} alt="Adobe InDesign" className="grid--icon"/>
+              <h3 className="grid--title">Adobe InDesign</h3>
+            </div>
+            <div className="grid--item">
+              <img src={figma} alt="Figma" className="grid--icon"/>
+              <h3 className="grid--title">Figma</h3>
+            </div>
+            <div className="grid--item">
+              <img src={xd} alt="Adobe Xd" className="grid--icon"/>
+              <h3 className="grid--title">Adobe Xd</h3>
+            </div>
+            <div className="grid--item">
+              <img src={lightroom} alt="Adobe Lightroom" className="grid--icon"/>
+              <h3 className="grid--title">Adobe Lightroom</h3>
+            </div>
+            <div className="grid--item">
+              <img src={procreate} alt="Procreate" className="grid--icon"/>
+              <h3 className="grid--title">Procreate</h3>
+            </div>
+            <div className="grid--item">
+              <img src={impozycjoner} alt="Impozycjoner" className="grid--icon"/>
+              <h3 className="grid--title">Impozycjoner</h3>
             </div>
           </div>
         </div>
